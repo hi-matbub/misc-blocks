0fc5aa14ba34ef58547a8ba1eb539edf439007ce
matbub <53072963+hi-matbub@users.noreply.github.com>
2021-03-17 20:57:10 -0700

    updated hashmap.ts

--- lib/hashmap.ts
+++ lib/hashmap.ts
@@ -1,18 +1,26 @@
 /**
  * 
  * @param arr 
- * @returns true if duplicate is found 
+ * @returns true on first duplicate found 
  */
 
 const hashmap = (arr: any) => {
-  const obj = {};
+  
+  const obj: {[index: string]: any} = {}
+
   for(let i = 0; i < arr.length; i += 1) {
     // if i matches item in object   
+
     if (obj[arr[i]]) {
+      console.log(`match located: ${arr[i]}`)
       return true;
     };
+
     // set key to true
     obj[arr[i]] = true;
   };
   return false;
 };
+
+hashmap([1,2,3,4,5,6,4]);
+hashmap([2,3,4,3,4]);
