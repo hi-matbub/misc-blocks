8a902db8b2d894ba813915971470a124907b3dca
matbub <53072963+hi-matbub@users.noreply.github.com>
2021-03-18 00:18:30 -0700

    Create find-sum-of-two-values.ts

---
+++ lib/find-sum-of-two-values.ts
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param numberOne
+ * @param numberTwo
+ * @returns sum
+ */
+const findSum = (numberOne: Number, numberTwo: Number) => Number(numberOne) + Number(numberTwo);
+
+console.log(findSum(20, 40));
