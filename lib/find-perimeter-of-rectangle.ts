3981d7a090e691a1271ab5a539f329cfbb7fd29c
matbub <53072963+hi-matbub@users.noreply.github.com>
2021-03-18 00:41:32 -0700

    Create find-perimeter-of-rectangle.ts

---
+++ lib/find-perimeter-of-rectangle.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param width 
+ * @param length 
+ * @returns Number
+ */
+
+const findPerimeterOfRectangle = (width: Number, length: Number) => (Number(width) * 2) + (Number(length) * 2)
+
+console.log(findPerimeterOfRectangle(6, 7));
+console.log(findPerimeterOfRectangle(56, 23));
+console.log(findPerimeterOfRectangle(34, 73));
+
